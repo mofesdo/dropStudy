73e0f46e0a7331438b8e5a8bdc1274115123a702
Glitch (radar-work-in-progress) <none>
2020-01-18 14:13:51 +0000

    🎆⚾️ Checkpoint ./liveSessions.html:2436003/782:3878407/20:10470821/150 ./script.js:2436003/1103

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-let request = new XMLHttpRequest()
+
+function myFunction(){
+ let request = new XMLHttpRequest()
 request.onreadystatechange = function() {
   if (this.readyState === 4) {
     console.log(this.status)
@@ -8,3 +10,5 @@ request.onreadystatechange = function() {
 request.open("POST", "https://events-api.notivize.com/applications/a8b922d9-7671-4fdf-8b12-b6b5f7547070/event_flows/12d6743e-51cd-404e-a748-6aaa7267dd3a/events", true)
 request.setRequestHeader("Content-Type", "application/json")
 request.send(JSON.stringify({"class": "ECON 101", "class_amount": "3", "student": "19513176514"}))
+}
+ alert("hello world");
