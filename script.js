5abb644846d7ef67ab3fa59e68a36e7b70259f4a
Glitch (hello-webpage) <none>
2020-01-18 09:44:45 +0000

    🍿🎟 Checkpoint ./index.html:2436003/44:10470821/12 ./script.js:2436003/699 ./cover_styles.css:10470821/38

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-curl https://api.radar.io/v1/users \
-  -H "Authorization: prj_live_sk_108c97506e7549bdbffd68cc4512a7df1fc1c469"
+let request = new XMLHttpRequest()
+request.onreadystatechange = function() {
+  if (this.readyState === 4) {
+    console.log(this.status)
+    console.log(this.responseText)
+  }
+}
+request.open("POST", "https://events-api.notivize.com/applications/5bf6be6b-68f2-4b9b-aa10-09725743fbe3/event_flows/fac86153-83a2-4292-ac18-7b252a223ef7/events", true)
+request.setRequestHeader("Content-Type", "application/json")
+request.send(JSON.stringify({"create": "<value for create>", "lifecycle_stage": "<value for lifecycle_stage>", "student": "<value for student>", "studier": "<value for studier>"}))
